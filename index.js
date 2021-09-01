7ac5b6d1dbd030bf87f135e57c325104958ec513
John Co <john.co@liferay.com>
2021-09-01 16:24:21 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+import Hello from './Hello';
+import './style.css';
+
+ReactDOM.render(<Hello name="world" />, document.getElementById('root'));
